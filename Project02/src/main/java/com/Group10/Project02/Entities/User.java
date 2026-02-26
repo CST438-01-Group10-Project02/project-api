fe8ece1a3c3fcf09b0747ea13be542681e556904
Peter Gloag <petergloag@gmail.com>
2026-02-25 17:53:11 -0800

    Added generic fields to User entity

--- Project02/src/main/java/com/Group10/Project02/Entities/User.java
+++ Project02/src/main/java/com/Group10/Project02/Entities/User.java
@@ -1,5 +1,55 @@
 package com.Group10.Project02.Entities;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+
+import java.util.Objects;
+
 @Entity
 public class User {
+    private @Id
+    @GeneratedValue long id;
+    private String username;
+
+    public User(){}
+    public User(String username){
+        this.username = username;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        User user = (User) o;
+        return id == user.id && Objects.equals(username, user.username);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, username);
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", username='" + username + '\'' +
+                '}';
+    }
 }
