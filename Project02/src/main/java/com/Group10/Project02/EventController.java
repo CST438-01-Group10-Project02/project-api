244155851285eda26e50feadbbd5c9c9ec07f22d
JesusAlfaroS <Jesusalfasua@gmail.com>
2026-02-25 20:01:55 -0800

    - Added EventController.java Warning: Errors and warnings due to unfinished files

---
+++ Project02/src/main/java/com/Group10/Project02/EventController.java
@@ -0,0 +1,53 @@
+package com.Group10.Project02;
+
+import com.Group10.Project02.entities.Event;
+import org.springframework.hateoas.EntityModel;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
+
+@RestController
+class EventController {
+    private final EventRepository repository;
+
+    EventController(EventRepository repository) {
+        this.repository = repository;
+    }
+
+    @PostMapping("/Events")
+    Event newEvent(@RequestBody Event newEvent) {
+        return repository.save(newEvent);
+    }
+
+    @GetMapping("/events")
+    List<Event> all() {
+        return repository.findAll();
+    }
+
+    @GetMapping("/events/{id}")
+    EntityModel<Event> getEvent(@PathVariable Long id) {
+        Event event = repository.findById(id)
+                .orElseThrow();
+
+        return EntityModel.of(event,
+                linkTo(methodOn(EventController.class).getEvent(id)).withSelfRel(),
+                linkTo(methodOn(EventController.class).all()).withRel("events")
+        );
+    }
+
+    @PutMapping("/events/{id}")
+    Event replaceEvent(@PathVariable Long id, @RequestBody Event newEvent) {
+        return repository.findById(id).map(event -> {
+            event.setName(newEvent.getName());
+            return repository.save(event);
+        }).orElseGet(()->{
+            return repository.save(newEvent);
+        });
+    }
+
+    @DeleteMapping("/event/{id}")
+    void deleteEvent(@PathVariable Long id) { repository.deleteById(id);}
+}
