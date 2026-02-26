c4cf3faaffc834dada0157baac72876ad2eb31ac
Peter Gloag <petergloag@gmail.com>
2026-02-25 18:05:18 -0800

    Finished UsersPageController.java

--- Project02/src/main/java/com/daclink/users/UsersPageController.java
+++ Project02/src/main/java/com/daclink/users/UsersPageController.java
@@ -1,6 +1,19 @@
 package com.daclink.users;
 
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
 public class UsersPageController {
 
     private final UsersRepository repository;
+
+    UsersPageController(UsersRepository repository){
+        this.repository = repository;
+    }
+
+    @GetMapping("/")
+    public String viewHomePage(Model model){
+        model.addAttribute("allUsers", repository.findAll());
+        return "index";
+    }
 }
