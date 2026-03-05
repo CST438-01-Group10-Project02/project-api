2c21dcafe97aa8a2fff310beb1458db5f67837c0
Peter Gloag <petergloag@gmail.com>
2026-03-05 14:34:06 -0800

    Ordered events by date. Added ability to query for events after a certain date

--- Project02/src/main/java/com/Group10/Project02/EventRepository.java
+++ Project02/src/main/java/com/Group10/Project02/EventRepository.java
@@ -1,37 +1,56 @@
 package com.Group10.Project02;
 
-import com.Group10.Project02.Entities.Event;
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
-import java.util.List;
+import com.Group10.Project02.Entities.Event;
 
 public interface EventRepository extends JpaRepository<Event, Long> {
 
-    // Singles
+    @Query("select e from Event e order by e.Date")
+    List<Event> findAllSortDate();
 
-    @Query("select e from Event e where e.Location = ?1")
+    // Singles
+    @Query("select e from Event e where e.Date > ?1 order by e.Date")
+    List<Event> findAfter(String date);
+
+    @Query("select e from Event e where e.Location = ?1 order by e.Date")
     List<Event> findByLocation(String location);
 
-    @Query("select e from Event e where e.Host.id = ?1")
+    @Query("select e from Event e where e.Host.id = ?1 order by e.Date")
     List<Event> findByHostId(Long id);
 
-    @Query("select e from Event e where e.Date = ?1")
+    @Query("select e from Event e where e.Date = ?1 order by e.Date")
     List<Event> findByDate(String date);
 
     // Doubles
-
-    @Query("select e from Event e where e.Location = ?1 and e.Host.id = ?2")
+    @Query("select e from Event e where e.Location = ?1 and e.Host.id = ?2 order by e.Date")
     List<Event> findByLocationHostId(String location, Long id);
 
-    @Query("select e from Event e where e.Location = ?1 and e.Date = ?2")
+    @Query("select e from Event e where e.Location = ?1 and e.Date = ?2 order by e.Date")
     List<Event> findByLocationDate(String location, String date);
 
-    @Query("select e from Event e where e.Host.id = ?1 and e.Date = ?2")
+    @Query("select e from Event e where e.Host.id = ?1 and e.Date = ?2 order by e.Date")
     List<Event> findByHostIdDate(Long id, String date);
 
-    // Triples
+    @Query("select e from Event e where e.Location = ?1 and e.Date > ?2 order by Date")
+    List<Event> findByLocationAfter(String location, String after);
 
-    @Query("select e from Event e where e.Location = ?1 and e.Host.id = ?2 and e.Date = ?3")
+    @Query("select e from Event e where e.Host.id = ?1 and e.Date > ?2 order by Date")
+    List<Event> findByHostIdAfter(Long id, String after);
+
+    // Triples
+    @Query("select e from Event e where e.Location = ?1 and e.Host.id = ?2 and e.Date = ?3 order by e.Date")
     List<Event> findByLocationHostIdDate(String location, Long id, String date);
+
+    @Query("select e from Event e where e.Location = ?1 and e.Host.id = ?2 and e.Date > ?3 order by e.Date")
+    List<Event> findByLocationHostId(String location, Long id, String after);
+
+    @Query("select e from Event e where e.Location = ?1 and e.Date = ?2 and e.Date > ?3 order by e.Date")
+    List<Event> findByLocationDate(String location, String date, String after);
+
+    @Query("select e from Event e where e.Host.id = ?1 and e.Date = ?2 and e.Date > ?3 order by e.Date")
+    List<Event> findByHostIdDate(Long id, String date, String after);
 }
--- Project02/src/main/java/com/Group10/Project02/LoadDatabase.java
+++ Project02/src/main/java/com/Group10/Project02/LoadDatabase.java
@@ -1,12 +1,13 @@
 package com.Group10.Project02;
 
-import com.Group10.Project02.Entities.Users;
-import com.Group10.Project02.Entities.Event;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import com.Group10.Project02.Entities.Event;
+import com.Group10.Project02.Entities.Users;
 
 @Configuration
 public class LoadDatabase {
@@ -17,8 +18,7 @@ public class LoadDatabase {
     CommandLineRunner initDatabase(UsersRepository usersRepository, EventRepository eventRepository) {
         return args -> {
             log.info("Preloading " + usersRepository.save(new Users("TestUser1")));
-            log.info("Preloading " + eventRepository.save(new Event("Super fun Party", usersRepository.findById(1L).orElseThrow(), "my house", "birthday", "3:30", "5:40", "02-26-2026")));
-            log.info("Preloading " + eventRepository.save(new Event("Come have fun", usersRepository.findById(1L).orElseThrow(), "Test", "You'll enjoy it", "now", "forever", "01-01-01")));
+            log.info("Preloading " + eventRepository.save(new Event("Super fun Party", usersRepository.findById(1L).orElseThrow(), "TestEvent", "Description", "12:00", "18:00", "2026-01-01")));
 
         };
     }
