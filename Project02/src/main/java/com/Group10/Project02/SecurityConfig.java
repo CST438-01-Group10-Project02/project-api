b079fb9e9e3654c82543b6fdc2456d6bcc84822d
JesusAlfaroS <Jesusalfasua@gmail.com>
2026-03-11 20:14:43 -0700

    Added Oauth and Security Dependencies Added SecurityConfig

---
+++ Project02/src/main/java/com/Group10/Project02/SecurityConfig.java
@@ -0,0 +1,27 @@
+package com.Group10.Project02;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.config.Customizer;
+
+
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.csrf(csrf -> csrf.disable())
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/public*/","/actuator/health").permitAll()
+                        .anyRequest().authenticated()
+                )
+                .oauth2ResourceServer(oauth2 ->
+                        oauth2.jwt(Customizer.withDefaults()));
+
+
+        return http.build();
+    }
+}
