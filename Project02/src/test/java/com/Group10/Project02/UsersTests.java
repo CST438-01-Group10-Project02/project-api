70ec673d0957ea68693d2e8f386a236cc1f19734
Peter Gloag <petergloag@gmail.com>
2026-03-12 15:12:25 -0700

    Fixed issue in UsersTests.java

--- Project02/src/test/java/com/Group10/Project02/UsersTests.java
+++ Project02/src/test/java/com/Group10/Project02/UsersTests.java
@@ -1,24 +1,23 @@
 package com.Group10.Project02;
 
-
-import com.Group10.Project02.Entities.Users;
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
-
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.client.RestTestClient;
-import org.springframework.web.context.WebApplicationContext;
-import tools.jackson.databind.ObjectMapper;
-
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import static org.springframework.test.web.servlet.setup.MockMvcBuilders.webAppContextSetup;
+import org.springframework.web.context.WebApplicationContext;
+
+import com.Group10.Project02.Entities.Users;
+
+import tools.jackson.databind.ObjectMapper;
 
 /**
  * @author Peter Gloag
@@ -26,8 +25,11 @@ import static org.springframework.test.web.servlet.setup.MockMvcBuilders.webAppC
  */
 @SpringBootTest
 public class UsersTests {
-    @Autowired private WebApplicationContext webContext;
-    @Autowired private UsersRepository repository;
+
+    @Autowired
+    private WebApplicationContext webContext;
+    @Autowired
+    private UsersRepository repository;
 
     private MockMvc mvc;
     @Autowired
@@ -43,6 +45,7 @@ public class UsersTests {
         mvc.perform(get("/users"))
                 .andExpect(status().isOk());
     }
+
     @Test
     void testGetUser() throws Exception {
         mvc.perform(get("/users/{id}", 1L))
@@ -50,6 +53,7 @@ public class UsersTests {
                 .andExpect(jsonPath("$.username").value("TestUser1"))
                 .andExpect(jsonPath("$.id").value(1));
     }
+
     @Test
     void testPostUser() throws Exception {
         Users testPostUser = new Users("TestPostUser");
@@ -58,12 +62,19 @@ public class UsersTests {
                 .andExpect(jsonPath("$.username").value(testPostUser.getUsername()))
                 .andExpect(jsonPath("$.id").value(2));
     }
+
     @Test
     void testPutUser() throws Exception {
         mvc.perform(put("/users/{id}", 1L).contentType(MediaType.APPLICATION_JSON).content("{\"username\":\"TestPostUser\"}"))
                 .andExpect(jsonPath("$.username").value("TestPostUser"))
                 .andExpect(jsonPath("$.id").value(1));
+
+        // Change back
+        mvc.perform(put("/users/{id}", 1L).contentType(MediaType.APPLICATION_JSON).content("{\"username\":\"TestUser1\"}"))
+                .andExpect(jsonPath("$.username").value("TestPostUser"))
+                .andExpect(jsonPath("$.id").value(1));
     }
+
     @Test
     void testGetUserByQuery() throws Exception {
         // Should get a list of users with username of "TestUser1"
