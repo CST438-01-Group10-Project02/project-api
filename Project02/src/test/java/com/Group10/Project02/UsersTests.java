fa4cfca94f34ea514e2dafc7154fd9e3eab616f6
Peter <petergloag@gmail.com>
2026-03-12 13:10:38 -0700

    Created unit tests for the Users object.

---
+++ Project02/src/test/java/com/Group10/Project02/UsersTests.java
@@ -0,0 +1,79 @@
+package com.Group10.Project02;
+
+
+import com.Group10.Project02.Entities.Users;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.client.RestTestClient;
+import org.springframework.web.context.WebApplicationContext;
+import tools.jackson.databind.ObjectMapper;
+
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
+import static org.springframework.test.web.servlet.setup.MockMvcBuilders.webAppContextSetup;
+
+/**
+ * @author Peter Gloag
+ * @since 3/11/2026
+ */
+@SpringBootTest
+public class UsersTests {
+    @Autowired private WebApplicationContext webContext;
+    @Autowired private UsersRepository repository;
+
+    private MockMvc mvc;
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    @BeforeEach
+    void setUp() {
+        mvc = webAppContextSetup(webContext).build();
+    }
+
+    @Test
+    void testUsersPageLoads() throws Exception {
+        mvc.perform(get("/users"))
+                .andExpect(status().isOk());
+    }
+    @Test
+    void testGetUser() throws Exception {
+        mvc.perform(get("/users/{id}", 1L))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.username").value("TestUser1"))
+                .andExpect(jsonPath("$.id").value(1));
+    }
+    @Test
+    void testPostUser() throws Exception {
+        Users testPostUser = new Users("TestPostUser");
+
+        mvc.perform(post("/users").contentType(MediaType.APPLICATION_JSON).content(objectMapper.writeValueAsString(testPostUser)))
+                .andExpect(jsonPath("$.username").value(testPostUser.getUsername()))
+                .andExpect(jsonPath("$.id").value(2));
+    }
+    @Test
+    void testPutUser() throws Exception {
+        mvc.perform(put("/users/{id}", 1L).contentType(MediaType.APPLICATION_JSON).content("{\"username\":\"TestPostUser\"}"))
+                .andExpect(jsonPath("$.username").value("TestPostUser"))
+                .andExpect(jsonPath("$.id").value(1));
+    }
+    @Test
+    void testGetUserByQuery() throws Exception {
+        // Should get a list of users with username of "TestUser1"
+        mvc.perform(get("/users").param("username", "TestUser1"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$[0].username").value("TestUser1"));
+
+        // Should return an empty list
+        mvc.perform(get("/users").param("username", "DummyValue"))
+                .andExpect(status().isOk())
+                .andExpect(content().string("[]"));
+    }
+}
