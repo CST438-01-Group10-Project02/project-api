73049104d3291ea3dce53cf987f95e8027647168
Peter <petergloag@gmail.com>
2026-03-15 19:27:30 -0700

    Fixed issue in UsersController.java

--- Project02/src/main/java/com/Group10/Project02/UsersController.java
+++ Project02/src/main/java/com/Group10/Project02/UsersController.java
@@ -5,14 +5,8 @@ import java.util.List;
 import org.springframework.hateoas.EntityModel;
 import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
 import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+
+import org.springframework.web.bind.annotation.*;
 
 import com.Group10.Project02.Entities.Users;
 
@@ -65,6 +59,7 @@ public class UsersController {
         });
     }
 
+    @CrossOrigin
     @DeleteMapping("/users/{id}")
     void deleteUser(@PathVariable Long id) {
         repository.deleteById(id);
